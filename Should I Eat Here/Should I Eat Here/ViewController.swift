a02916e41e7ae674350b244373f557403f7dc2fc
benbueltmann <benbueltmann@gmail.com>
2018-10-17 14:21:13 -0500

    Add project files

---
+++ Should I Eat Here/Should I Eat Here/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Should I Eat Here
+//
+//  Created by Ben Bueltmann on 10/17/18.
+//  Copyright © 2018 Arrested Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
