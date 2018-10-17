3bc5ae7c3b4029c912b10620f418ed9564d8c7f2
benbueltmann <benbueltmann@gmail.com>
2018-10-17 14:31:57 -0500

    add model file, update .gitignore

---
+++ Should I Eat Here/Should I Eat Here/Model/Restaurant.swift
@@ -0,0 +1,9 @@
+//
+//  Restaurant.swift
+//  Should I Eat Here
+//
+//  Created by Ben Bueltmann on 10/17/18.
+//  Copyright © 2018 Arrested Development. All rights reserved.
+//
+
+import Foundation
